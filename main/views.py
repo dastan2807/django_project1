0de6cc17dc272745f2c7f5e4ae89ef759f5fa7cf
Dastan <bakytbekov.dastan.2000@gmail.com>
2021-04-30 13:55:02 +0600

    update templates

--- main/views.py
+++ main/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+def main(request):
+    context = {
+
+    }
+    return render(request, 'index.html', context)
+
+def bank(request):
+    context = {
+
+    }
+    return render(request, 'bank.html', context)
+def internalCode(request):
+    context = {
+
+    }
+    return render(request, 'InternalCode.html', context)
